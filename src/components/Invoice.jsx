0a19441e0d605c40b7cdc370dbc7bc6fad98e5a7
shivamdayma <shivamdayma19@gmail.com>
2025-10-07 17:38:06 +0530

    things has been updated

--- src/components/Invoice.jsx
+++ src/components/Invoice.jsx
@@ -3,12 +3,14 @@ const Invoice = ({ data }) => {
     <div className="relative w-[210mm] h-[297mm] mx-auto shadow-lg">
       <img src="form.jpg" className='w-full h-full' alt="" />
       <div className='mx-auto m-5 px-8 py-20 z-10 absolute top-40'>
+        <p>Invoice No:{data.invoiceNo} </p>
+        <p className="mb-5">Data: {data.date} </p>
         <p className='font-bold'>Billed To:</p>
         <p>Name: {data.name}</p>
-        <p>Mobile: +91 9907472038 </p>
-        <p className='mb-10'>Address: Bull streat , Bombay </p>
-        <h3 className='font-bold'>Description of services:</h3>
-        <div className='overflow-x-auto  mb-10'>
+        <p>Mobile: {data.mobileNumber}</p>
+        <p className='mb-5'>Address: {data.address}</p>
+        <h3 className='font-bold'>Description of services:{data.productdescription}</h3>
+        <div className='overflow-x-auto  mb-5'>
           <table className='w-full table-auto border-collapse border'>
             <thead>
               <tr className='bg-gray-100'>
@@ -23,41 +25,47 @@ const Invoice = ({ data }) => {
               </tr>
             </thead>
             <tbody>
+     {data.productRows && data.productRows.length > 0 && data.productRows.map((row, index) => (
+         
               <tr>
-                <td className='border px-2 py-1'>1</td>
+                <td className='border px-2 py-1'>{index + 1}</td>
 
                 <td className='border px-2 py-1'>
-
+                  {row.description}
                 </td>
                 <td className='border px-2 py-1 text-right'>
-
+                  {row.hsnSac}
                 </td>
                 <td className='border px-2 py-1 text-right'>
-
+                  {row.quantity}
                 </td>
                 <td className='border px-2 py-1 text-right'>
-
+                  {row.taxableValue}
                 </td>
                 <td className='border px-2 py-1 text-right'>
-
+                  {row.cgst}
                 </td>
                 <td className='border px-2 py-1 text-right'>
-
+                  {row.sgst}
+                </td>
+                <td className='border px-2 py-1 text-right'>
+                  {row.total}
                 </td>
               </tr>
+              ))}
             </tbody>
           </table>
         </div>
 
 
         <p>
-          Total Invoice Amount (incl.GST):
+          Total Invoice Amount (incl.GST):{data.amount}
         </p>
-        <p>Amount in words :199Rs</p>
-        <p>Amount Paid :199Rs</p>
-        <p className='mb-8'>Amount Due : None</p>
+        <p>Amount in words :{data.ainw}</p>
+        <p>Amount Paid :{data.amountPaid}</p>
+        <p className='mb-5'>Amount Due : {data.amountdue}</p>
 
-        <h2 className='font-bold'>Additional Notes :</h2>
+        <h2 className='font-bold'>Additional Notes : {}</h2>
         <p>-Amount include GST @18%</p>
         <p className='mb-5'>-Nodues pending</p>
       </div>
